1e5d4bbfc9a4f1f22d3adafc068d7a5c14c6fec2
GuyGamliel12 <94290605+GuyGamliel12@users.noreply.github.com>
2023-07-15 20:29:35 +0300

    Update settings.py

--- code/server/src/asap/asap/settings.py
+++ code/server/src/asap/asap/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=i*@53k#w8jpl^^2b)jtl7+b@(#g%s=xnw-)*!@y0na)uu($25'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
